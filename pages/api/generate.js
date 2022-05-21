6b605143bebaabffea721ac9195a80a904f24e25
Joshua J. Haller <phone@joshuajhaller.com>
2022-05-21 10:32:36 +1000

    Cleaned up commented out code.

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,48 +1,3 @@
-// const { Configuration, OpenAIApi } = require("openai");
-
-// const configuration = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-
-// const openai = new OpenAIApi(configuration);
-
-// export default async function (req, res) {
-//   const completion = await openai.createCompletion("text-curie-001", {
-//     prompt: generatePrompt(req.body.user),
-//     temperature: 0.7,
-//     max_tokens: 200,
-//     top_p: 1,
-//     frequency_penalty: 0,
-//     presence_pentalty: 0,
-//   });
-//   res.status(200).json({ result: completion.data.choices[0].text });
-// }
-
-// function generatePrompt(user) {
-//   return `"What was the first empire in history?\n\nThe first empire in history was the Assyrian Empire.\n\nWho succeeded them?\n\nThe succeeding empire was the Persian Empire.\n\nNot the Babylonians?\n\nThe Babylonians did not succeed the Assyrian Empire, but were a predecessor of that empire.\n\nDid Babylon have an empire?\n\nNo, Babylon did not have an empire.\n\nWhat about Egypt?\n\nEgypt was not an empire, but had a long history of being an influential culture and power in the region.\n\nWhy was Assyria an empire?\n\nThe Assyrian Empire was an empire because it was a large and powerful kingdom that controlled a large area of the world.",`;
-// }
-
-// const openai = new OpenAI(process.env.OPENAI_API_KEY);
-
-// export default async (req, res) => {
-//   let prompt = `Artist: ${req.body.name}\n\nLyrics:\n`;
-//   const gptResponse = await openai.complete({
-//     engine: "davinci",
-//     prompt: prompt,
-//     maxTokens: 50,
-//     temperature: 0.7,
-//     topP: 1,
-//     presencePenalty: 0,
-//     frequencyPenalty: 0.5,
-//     bestOf: 1,
-//     n: 1,
-//   });
-
-//   res.status(200).json({ text: `${gptResponse.data.choices[0].text}` });
-// };
-
-//Just trying to get the process going
-
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
@@ -60,24 +15,5 @@ export default async function (req, res) {
 }
 
 function generatePrompt(user) {
-  // const capitalizedAnimal =
-  //   animal[0].toUpperCase() + animal.slice(1).toLowerCase();
   return `I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What was the first empire in history?\nA: The first empire in history was the Assyrian Empire\n\nQ: Who succeeded them?\nA: The succeeding empire was the Persian Empire.\n\nQ: Not the Babylonians?\nA: The Babylonians did not succeed the Assyrian Empire, but were a predecessor of that empire.\n\nQ: Did Babylon have an empire?\nA: No, Babylon did not have an empire.\n\nQ: What about Egypt?\nA: Egypt was not an empire, but had a long history of being an influential culture and power in the region.\n\nQ: Why was Assyria an empire?\nA: The Assyrian Empire was an empire because it was a large and powerful kingdom that controlled a large area of the world\n\nQ: Who came after the Persians?\n`;
 }
-
-// const { Configuration, OpenAIApi } = require("openai");
-
-// const configuration = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-// const openai = new OpenAIApi(configuration);
-
-// const response = await openai.createCompletion("text-curie-001", {
-//   prompt: "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What was the first empire in history?\nA: The first empire in history was the Assyrian Empire\n\nQ: Who succeeded them?\nA: The succeeding empire was the Persian Empire.\n\nQ: Not the Babylonians?\nA: The Babylonians did not succeed the Assyrian Empire, but were a predecessor of that empire.\n\nQ: Did Babylon have an empire?\nA: No, Babylon did not have an empire.\n\nQ: What about Egypt?\nA: Egypt was not an empire, but had a long history of being an influential culture and power in the region.\n\nQ: Why was Assyria an empire?\nA: The Assyrian Empire was an empire because it was a large and powerful kingdom that controlled a large area of the world\n\nQ: Who came after the Persians?\n",
-//   temperature: 0,
-//   max_tokens: 100,
-//   top_p: 1,
-//   frequency_penalty: 0,
-//   presence_penalty: 0,
-//   stop: ["\n"],
-// });
--- pages/index.js
+++ pages/index.js
@@ -1,113 +1,6 @@
-// import Head from "next/head";
-// import react, { useState } from "react";
-// //modify or update
-// import styles from "../styles/Home.module.css";
-// // import  from "react";
-// // import { useState, useEffect } from "react";
-
-// export default function Home() {
-//   const [userInput, setUserInput] = useState("");
-//   const [result, setResult] = useState();
-
-//   async function onSubmit(event) {
-//     event.preventDefault();
-//     const response = await fetch("/api/generate", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({ user: userInput }),
-//     });
-//     const data = await response.json();
-//     setResult(data.result);
-//     setUserInput("");
-//   }
-
-//   return (
-//     <div>
-//       <h1>History lessons</h1>
-//       <form onSubmit={onSubmit}>
-//         <input
-//           type="text"
-//           name="user"
-//           placeholder="What would you like to know about history?"
-//           value={userInput}
-//           onChange={(e) => setUserInput(e.target.value)}
-//         />
-//         <input type="submit" value="Learn!" />
-//       </form>
-//     </div>
-//   );
-//   console.log(userInput);
-// }
-
-// export default function Home() {
-//   const [data, setData] = useState({ text: "" });
-//   const [query, setQuery] = useState();
-//   const [search, setSearch] = useState();
-//   const [isLoading, setIsLoading] = useState(false);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       if (search) {
-//         setIsLoading(true);
-//         const res = await fetch(`/api/openai`, {
-//           body: JSON.stringify({
-//             name: search,
-//           }),
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//           method: "POST",
-//         });
-//         const data = await res.json();
-//         setData(data);
-//         setIsLoading(false);
-//       }
-//     };
-
-//     fetchData();
-//   }, [search]);
-//   return (
-//     <div className={styles.container}>
-//       <Head>
-//         <title>GPT-3 App</title>
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
-
-//       <main className={styles.main}>
-//         <h1 className={styles.title}>
-//           <a>AI Lyrics Generator</a>
-//         </h1>
-
-//         <p className={styles.description}>Built with NextJS & GPT-3 API</p>
-
-//         <div className={styles.grid}>
-//           <div className={styles.card}>
-//             <h3>Enter Artist:</h3>
-//             <input
-//               type="text"
-//               value={query}
-//               onChange={(event) => setQuery(event.target.value)}
-//             />
-//             <button type="button" onClick={() => setSearch(query)}>
-//               Generate
-//             </button>
-
-//             <h4>Lyrics</h4>
-//             {isLoading ? <div>Loading ...</div> : <span>{data.text}</span>}
-//           </div>
-//         </div>
-//       </main>
-//     </div>
-//   );
-// }
-
-//just trying to get process going
 import Head from "next/head";
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import styles from "./index.module.css";
 
 export default function Home() {
   const [userInput, setUserInput] = useState("");
@@ -129,13 +22,6 @@ export default function Home() {
 
   return (
     <div>
-      {/* <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
-      </Head> */}
-
-      {/* <main className={styles.main}> */}
-      {/* <img src="/dog.png" className={styles.icon} /> */}
       <h3>Find out about early empires!</h3>
       <form onSubmit={onSubmit}>
         <input
@@ -148,7 +34,6 @@ export default function Home() {
         <input type="submit" value="Find out" />
       </form>
       <div>{result}</div>
-      {/* </main> */}
     </div>
   );
 }
