d9ac24d443b6e6b1d0f4a1dba235e57230663c32
Joshua J. Haller <phone@joshuajhaller.com>
2022-05-21 11:03:21 +1000

    Updated temperature for OpenAI prompt. Reworked the form to use Bootstrap.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Form, Button } from "react-bootstrap";
 
 export default function Home() {
   const [userInput, setUserInput] = useState("");
@@ -21,9 +22,23 @@ export default function Home() {
   }
 
   return (
-    <div>
-      <h3>Find out about early empires!</h3>
-      <form onSubmit={onSubmit}>
+    <Container>
+      {/* <div> */}
+      {/* <h3>Find out about early empires!</h3> */}
+      <Form onSubmit={onSubmit}>
+        <Form.Group className="mb-3" controlId="formUserQuestion">
+          <Form.Label>Early Empire History</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Ask a question about ancient empires"
+          />
+          <Form.Text className="text-muted">Best start with Persian.</Form.Text>
+        </Form.Group>
+        <Button varient="primary" type="submit">
+          Ask
+        </Button>
+      </Form>
+      {/* <form onSubmit={onSubmit}>
         <input
           type="text"
           name="question"
@@ -32,8 +47,9 @@ export default function Home() {
           onChange={(e) => setUserInput(e.target.value)}
         />
         <input type="submit" value="Find out" />
-      </form>
+      </form> */}
       <div>{result}</div>
-    </div>
+      {/* </div> */}
+    </Container>
   );
 }
